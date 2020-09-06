d5abc1d7cf10e67fa1bb853cd73a22384802726f
Mathias Schaefer (molily) <molily@mailbox.org>
2020-09-06 22:09:10 +0200

    ThresholdWarningDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ThresholdWarningDirective } from './threshold-warning.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ThresholdWarningDirective
   ],
   imports: [
     BrowserModule
