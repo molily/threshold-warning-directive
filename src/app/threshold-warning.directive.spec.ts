6bd7b798d8cc7d79e8b45d37b02444c2841edd94
Mathias Schaefer (molily) <molily@mailbox.org>
2020-11-14 22:56:14 +0100

    Use async/await

--- src/app/threshold-warning.directive.spec.ts
+++ src/app/threshold-warning.directive.spec.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { findEl, setFieldValue } from './spec-helpers/element.spec-helper';
 import { ThresholdWarningDirective } from './threshold-warning.directive';
@@ -15,11 +15,11 @@ describe('ThresholdWarningDirective', () => {
   let fixture: ComponentFixture<HostComponent>;
   let input: HTMLInputElement;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ThresholdWarningDirective, HostComponent],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HostComponent);
